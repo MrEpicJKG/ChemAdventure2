acbf40af872a65defbc00fa4fbe1eeea467cf437
MrEpicJKG <johnkgerkeniv@gmail.com>
2022-05-09 15:12:26 -0400

    Working on touch Controls

--- Assets/3rd Party Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/3rd Party Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,102 +7,97 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
+        [SerializeField] private bool useAxis = true;
         private PlatformerCharacter2D m_Character;
         [HideInInspector] public bool m_Jump;
         [HideInInspector] public bool shouldGoLeft;
         [HideInInspector] public bool shouldGoRight;
         [HideInInspector] public bool shouldCrouch;
 
+        CrossPlatformInputManager.VirtualAxis m_Axis;
+
 
         private void Awake()
         {
             m_Character = GetComponent<PlatformerCharacter2D>();
+            m_Axis = CrossPlatformInputManager.VirtualAxisReference("Horizontal");
         }
 
 
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                if (Input.GetKeyDown(KeyCode.W) == true || CrossPlatformInputManager.GetButtonDown("Jump"))
-                {
-                    m_Jump = true;
-                }
-                else
-                {
-                    m_Jump = false;
-				}
-            }
+        //private void Update()
+        //{
+     //       if (!m_Jump)
+     //       {
+     //           // Read the jump input in Update so button presses aren't missed.
+     //           if (Input.GetKeyDown(KeyCode.W) == true || CrossPlatformInputManager.GetButtonDown("Jump"))
+     //           {
+     //               m_Jump = true;
+                    
+     //           }
+     //       }
 
-            //if (!shouldGoLeft)
-            //{
-                // Read the jump input in Update so button presses aren't missed.
-                if (Input.GetKeyDown(KeyCode.A) == true || CrossPlatformInputManager.GetButtonDown("Left"))
-                {
-                    shouldGoLeft = true;
-                    shouldGoRight = false;
-                }
-                else if (Input.GetKeyUp(KeyCode.A) == true || CrossPlatformInputManager.GetButtonUp("Left"))
-                {
-                    shouldGoLeft = false;
-                }
-            //}
+     //           if (Input.GetKeyDown(KeyCode.A) == true || CrossPlatformInputManager.GetButtonDown("Left"))
+     //           {
+     //               shouldGoLeft = true;
+     //               shouldGoRight = false;
+     //           }
+     //           else if (Input.GetKeyUp(KeyCode.A) == true || CrossPlatformInputManager.GetButtonUp("Left"))
+     //           {
+     //               shouldGoLeft = false;
+     //           }
 
-           // if (!shouldGoRight)
-           // {
-                // Read the jump input in Update so button presses aren't missed.
-                if (Input.GetKeyDown(KeyCode.D) == true || CrossPlatformInputManager.GetButtonDown("Right"))
-                {
-                    shouldGoRight = true;
-                    shouldGoLeft = false;
-                }
-                else if (Input.GetKeyUp(KeyCode.D) == true || CrossPlatformInputManager.GetButtonUp("Right"))
-                {
-                    shouldGoRight = false;
-                }
-           // }
+     //           if (Input.GetKeyDown(KeyCode.D) == true || CrossPlatformInputManager.GetButtonDown("Right"))
+     //           {
+     //               shouldGoRight = true;
+     //               shouldGoLeft = false;
+     //           }
+     //           else if (Input.GetKeyUp(KeyCode.D) == true || CrossPlatformInputManager.GetButtonUp("Right"))
+     //           {
+     //               shouldGoRight = false;
+     //           }
 
-            if (!shouldCrouch)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                if (Input.GetKeyDown(KeyCode.S) == true || CrossPlatformInputManager.GetButtonDown("Crouch"))
-                {
-                    if(shouldCrouch == true)
-                    {
-                        shouldCrouch = false;
-					}
-                    else
-                    {
-                        shouldCrouch = true;
-					}
-                }
-            }
 
-        }
+     //           if (Input.GetKeyDown(KeyCode.S) == true || CrossPlatformInputManager.GetButtonDown("Crouch"))
+     //           {
+     //               if(shouldCrouch == true)
+     //               {
+     //                   shouldCrouch = false;
+					//}
+     //               else
+     //               {
+     //                   shouldCrouch = true;
+					//}
+     //           }
+     //   }
 
 
         private void FixedUpdate()
         {
             // Read the inputs.
             float h = 0;
-            if(shouldGoRight == true)
+            if (useAxis == false)
             {
-                h = 1;
-			}
-            else if(shouldGoLeft == true)
-            {
-                h = -1;
-			}
+                if (shouldGoRight == true)
+                {
+                    h = 1;
+                }
+                else if (shouldGoLeft == true)
+                {
+                    h = -1;
+                }
+                else
+                {
+                    h = 0;
+                }
+            }
             else
             {
-                h = 0;
+                h = m_Axis.GetValue;
 			}
             // Pass all parameters to the character control script.
             m_Character.Move(h, shouldCrouch, m_Jump);
-            print("h: " + h.ToString() + "   Left: " + shouldGoLeft + "   Right: " + shouldGoRight + "   Crouch: " + shouldCrouch.ToString() + "   Jump: " + m_Jump);
+            //print("h: " + h.ToString() + "   Left: " + shouldGoLeft + "   Right: " + shouldGoRight + "   Crouch: " + shouldCrouch.ToString() + "   Jump: " + m_Jump);
             m_Jump = false;
-            shouldCrouch = false;
         }
     }
 }
--- Assets/Scripts/TouchInputManager.cs
+++ Assets/Scripts/TouchInputManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets._2D;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class TouchInputManager : MonoBehaviour
+public class TouchInputManager : MonoBehaviour//, IPointerDownHandler, IPointerUpHandler;
 {
     [SerializeField] private RectTransform controlsSafetyRectTrans;
     [SerializeField] private RectTransform leftRectTrans;
@@ -12,58 +14,112 @@ public class TouchInputManager : MonoBehaviour
 
 	void Update()
     {
-        Touch[] touches = Input.touches;
+
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMin, controlsSafetyRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMin, controlsSafetyRectTrans.rect.yMax))aaaaaa, Color.white);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMin, controlsSafetyRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMax, controlsSafetyRectTrans.rect.yMin)), Color.white);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMin, controlsSafetyRectTrans.rect.yMax)), Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMax, controlsSafetyRectTrans.rect.yMax)), Color.white);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMax, controlsSafetyRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(controlsSafetyRectTrans.rect.xMax, controlsSafetyRectTrans.rect.yMax)), Color.white);
+
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMin, rightRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMin, rightRectTrans.rect.yMax)), Color.green);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMin, rightRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMax, rightRectTrans.rect.yMin)), Color.green);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMin, rightRectTrans.rect.yMax)), Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMax, rightRectTrans.rect.yMax)), Color.green);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMax, rightRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(rightRectTrans.rect.xMax, rightRectTrans.rect.yMax)), Color.green);
+
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMin, leftRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMin, leftRectTrans.rect.yMax)), Color.blue);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMin, leftRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMax, leftRectTrans.rect.yMin)), Color.blue);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMin, leftRectTrans.rect.yMax)), Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMax, leftRectTrans.rect.yMax)), Color.blue);
+		//Debug.DrawLine(Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMax, leftRectTrans.rect.yMin)), Camera.main.ScreenToViewportPoint(new Vector2(leftRectTrans.rect.xMax, leftRectTrans.rect.yMax)), Color.blue);
+		Touch[] touches = Input.touches;
         bool isLRDown = false;
         foreach(Touch touch in touches)
         {
-            Vector3 pos = touch.position;
+            Vector3 pos = Camera.main.ScreenToViewportPoint(touch.position);
+
+            Debug.DrawLine(new Vector2(pos.x - 0.5f, pos.y), new Vector2(pos.x + 0.5f, pos.y), Color.red);
+            Debug.DrawLine(new Vector2(pos.x, pos.y - 0.5f), new Vector2(pos.x, pos.y + 0.5f), Color.red);
+
             if (controlsSafetyRectTrans.rect.Contains(pos) == false)
             {
                 RaycastHit2D hit;
-                Ray ray = Camera.main.ScreenPointToRay(pos);
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 hit = Physics2D.Raycast(ray.origin, ray.direction);
-                ObjectInteractController oic = hit.transform.GetComponent<ObjectInteractController>();
-                if (oic != null)
+                if (hit.transform != null)
                 {
-                    oic.OnTapped();
-				}
+                    ObjectInteractController oic = hit.transform.GetComponent<ObjectInteractController>();
+                    if (oic != null)
+                    {
+                        oic.OnTapped();
+                    }
+                }
             }
-            else
-            {
-                if(leftRectTrans.rect.Contains(pos) == true || rightRectTrans.rect.Contains(pos) == true)
-                {
-                    isLRDown = true;
-				}
-			}
+   //         else
+   //         {
+   //             //Detect and React to control button presses
+   //             if(touch.phase == TouchPhase.Began)
+   //             {
+   //                 if(rightRectTrans.rect.Contains(Camera.main.ScreenToViewportPoint(touch.position)) == true) //go right
+   //                 {
+   //                     controls.shouldGoRight = true;
+			//		}
+   //                 else if(leftRectTrans.rect.Contains(Camera.main.ScreenToViewportPoint(touch.position)) == true) //go left
+   //                 {
+   //                     controls.shouldGoLeft = true;
+			//		}
+   //                 Debug.Log("Touch Started");
+			//	}
+   //             else if(touch.phase == TouchPhase.Ended)
+   //             {
+   //                 if (rightRectTrans.rect.Contains(Camera.main.ScreenToViewportPoint(touch.position)) == true) //go right
+   //                 {
+   //                     controls.shouldGoRight = false;
+   //                 }
+   //                 else if (leftRectTrans.rect.Contains(Camera.main.ScreenToViewportPoint(touch.position)) == true) //go left
+   //                 {
+   //                     controls.shouldGoLeft = false;
+   //                 }
+   //                 Debug.Log("Touch Ended");
+   //             }
+			//}
 		}
 
         
     }
 
-    public void OnJumpPress()
-    {
-        controls.m_Jump = true;
-        print("jump called");
+	public void OnJumpPress()
+	{
+		controls.m_Jump = true;
+		print("jump called");
 	}
 
-    public void OnLeftPress()
-    {
-        controls.shouldGoLeft = true;
-        controls.shouldGoRight = false;
-        print("left called");
-	}
+	//   public void OnLeftPress()
+	//   {
+	//       controls.shouldGoLeft = true;
+	//       controls.shouldGoRight = false;
+	//       print("left called");
+	//}
 
-    public void OnRightPress()
-    {
-        controls.shouldGoRight = true;
-        controls.shouldGoLeft = false;
-        print("right called");
-	}
+	//   public void OnRightPress()
+	//   {
+	//       controls.shouldGoRight = true;
+	//       controls.shouldGoLeft = false;
+	//       print("right called");
+	//}
 
-    public void OnDownPress()
-    {
-        controls.shouldCrouch = true;
+	public void OnDownPress()
+	{
+        if (controls.shouldCrouch == true)
+        {
+            controls.shouldCrouch = false;
+        }
+        else
+        {
+            controls.shouldCrouch = true;
+        }
         print("down called");
+
+        
 	}
+
+    
 
 }
