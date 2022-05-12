a9443435f005960c4253a88457e448b2219c38d8
MrEpicJKG <johnkgerkeniv@gmail.com>
2022-05-12 12:49:43 -0400

    Did a little work on the UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float currHealth = 100;
+    public float maxHealth = 100;
+    public int currMoney = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
